fdb31a1dfacd86ffbc51a40567a89a8eebf2c027
Edi Modrić <edi@netgen.hr>
2015-08-28 14:32:29 +0200

    Fix namespace in ToleraterIOService

--- src/Ez/Summer/CampBundle/Override/ToleranterIOService.php
+++ src/Ez/Summer/CampBundle/Override/ToleranterIOService.php
@@ -6,7 +6,7 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-namespace eZ\Summer\CampBundle\Override;
+namespace Ez\Summer\CampBundle\Override;
 
 use eZ\Publish\Core\IO\TolerantIOService;
 use eZ\Publish\Core\IO\Exception\BinaryFileNotFoundException;
